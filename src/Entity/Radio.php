2f589366e93a063b37bb2e840f5df27a02680fa3
frian <a@frian.org>
2020-08-27 15:49:33 +0200

    added Radio

---
+++ src/Entity/Radio.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RadioRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RadioRepository::class)
+ */
+class Radio
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=512)
+     */
+    private $streamUrl;
+
+    /**
+     * @ORM\Column(type="string", length=512, nullable=true)
+     */
+    private $homepageUrl;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStreamUrl(): ?string
+    {
+        return $this->streamUrl;
+    }
+
+    public function setStreamUrl(string $streamUrl): self
+    {
+        $this->streamUrl = $streamUrl;
+
+        return $this;
+    }
+
+    public function getHomepageUrl(): ?string
+    {
+        return $this->homepageUrl;
+    }
+
+    public function setHomepageUrl(?string $homepageUrl): self
+    {
+        $this->homepageUrl = $homepageUrl;
+
+        return $this;
+    }
+}
