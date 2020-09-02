58d8aa8ecd12bacbdc7cdf066f70480148f516b7
frian <a@frian.org>
2020-09-02 20:51:30 +0200

    added ThemeHelper and Twig helper

---
+++ src/Helper/ThemeHelper.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Helper;
+
+/**
+ * class representing a weekly calendar
+ */
+class ThemeHelper {
+
+    /**
+	 * Entity Manager
+	 *
+	 * @var EntityManager $em
+	 */
+	protected $em;
+
+	/**
+	 * Constructor
+	 *
+	 * @param EntityManager $em
+	 */
+    public function __construct(\Doctrine\ORM\EntityManager $em, $securityContext) {
+
+ 		$this->em = $em;
+ 		$this->context = $securityContext;
+ 	}
+
+    /**
+     * get user theme
+     *
+     * @return string $theme
+     */
+    public function get() {
+
+        $config = $this->em->getRepository('App:Config')->find(1);
+        $theme = $config->getTheme();
+
+        return $theme;
+    }
+}
---
+++ src/Twig/FileExistsExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+use Symfony\Component\Filesystem\Filesystem;
+
+class FileExistsExtension extends AbstractExtension
+{
+    private $fileSystem;
+    private $projectDir;
+
+    public function __construct(Filesystem $fileSystem, string $projectDir)
+    {
+        $this->fileSystem = $fileSystem;
+        $this->projectDir = $projectDir;
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('file_exists', [$this, 'fileExists']),
+        ];
+    }
+
+    /**
+     * @param string An absolute or relative to public folder path
+     *
+     * @return bool True if file exists, false otherwise
+     */
+    public function fileExists(string $path): bool
+    {
+        if (!$this->fileSystem->isAbsolutePath($path)) {
+            $path = "{$this->projectDir}/web/{$path}";
+        }
+
+        return $this->fileSystem->exists($path);
+    }
+}
