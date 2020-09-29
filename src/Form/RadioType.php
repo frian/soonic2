4d38527a435823e049ea8ba4e35c756749fba601
frian <a@frian.org>
2020-09-29 22:43:09 +0200

    added missing translations

--- src/Form/RadioType.php
+++ src/Form/RadioType.php
@@ -12,9 +12,9 @@ class RadioType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('streamUrl')
-            ->add('homepageUrl')
+            ->add('name', null , array('label' => 'name'))
+            ->add('streamUrl', null , array('label' => 'streamUrl'))
+            ->add('homepageUrl', null , array('label' => 'homePage'))
         ;
     }
 
