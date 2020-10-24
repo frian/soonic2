78b31f8213d037c371bc36a1cf9590f199d21963
frian <a@frian.org>
2020-10-24 16:11:55 +0200

    moved code to main.js

--- public/js/album.js
+++ public/js/album.js
@@ -1,18 +1,18 @@
 $(function() {
 
-    const observer = lozad();
-    observer.observe();
-
-    $status = 'hidden';
-    $(document).on("click", ".album-container-content", function(e) {
-        status = status == 'hidden' ? 'visible' : 'hidden';
-        console.log('clic');
-        if (status == 'hidden') {
-            $(this).children(".lozad").fadeOut('fast');
-        }
-        else {
-            $(this).children(".lozad").fadeIn('fast');
-        }
-    });
+    // const observer = lozad();
+    // observer.observe();
+    //
+    // $status = 'hidden';
+    // $(document).on("click", ".album-container-content", function(e) {
+    //     status = status == 'hidden' ? 'visible' : 'hidden';
+    //     console.log('clic');
+    //     if (status == 'hidden') {
+    //         $(this).children(".lozad").fadeOut('fast');
+    //     }
+    //     else {
+    //         $(this).children(".lozad").fadeIn('fast');
+    //     }
+    // });
 
 });
