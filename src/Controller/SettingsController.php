5b46c1f4e015a9da217962340226dfcbb51ff463
frian <a@frian.org>
2022-06-04 13:56:29 +0200

    fixed doctrine upgrade bug - fixed symfony form upgrade bug

--- src/Controller/SettingsController.php
+++ src/Controller/SettingsController.php
@@ -30,8 +30,8 @@ class SettingsController extends AbstractController
         foreach ($tables as $table) {
             $query = "select max(id) from $table";
             $statement = $em->getConnection()->prepare($query);
-            $statement->execute();
-            $result = $statement->fetch()['max(id)'];
+            $result = $statement->execute();
+            $result = $result->fetchAssociative()['max(id)'];
             if ($result === null) {
                 $result = 0;
             }
@@ -39,13 +39,16 @@ class SettingsController extends AbstractController
         }
 
         // -- get config form
-        $config = $em->getRepository('App:Config')->find(1);
+        $config = $em->getRepository('App\Entity\Config')->find(1);
+        if (! $config) {
+            die;
+        }
         $editForm = $this->createForm('App\Form\ConfigType', $config);
         $editForm->handleRequest($request);
 
-        return $this->render('settings/index.html.twig', [
+        return $this->renderForm('settings/index.html.twig', [
             'infos' => $infos,
-            'edit_form' => $editForm->createView(),
+            'edit_form' => $editForm,
         ]);
     }
 }
